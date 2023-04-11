2609022046d1e8898e44b485c99c35e3c09e393c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-11 16:14:34 +0530

    feat: add region filter select to HomePage
    This commit adds a select dropdown menu to filter countries by region. The `select` element is added to the `header` section along with the title. The selected region state is managed using the `useCountryStore` hook. The available options in the `select` element are "All", "America", "Oceania", and "Asia".
    
    The list of countries is filtered based on the selected region. The `handleRegionSelect` function is called whenever a region is selected from the dropdown.
    
    This commit also updates the `handleCountryClick` function to set the `selectedCountry` state using the `setSelectedCountry` function from the `useCountryStore` hook.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { useCountryStore } from "@/lib/state/country-store";
 
 const API_BASE_URL = "https://restcountries.com/v3.1";
 
@@ -22,6 +23,22 @@ export default function HomePage() {
     queryFn: () => fetcher(`${API_BASE_URL}/all`),
   });
 
+  const state = useCountryStore();
+  const {
+    selectedCountry,
+    setSelectedCountry,
+    selectedRegion,
+    setSelectedRegion,
+  } = state;
+
+  const handleCountryClick = (countryName: string) => {
+    setSelectedCountry(countryName);
+  };
+
+  const handleRegionSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedRegion(e.currentTarget.value);
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -33,13 +50,36 @@ export default function HomePage() {
   return (
     <>
       <section>
-        <h1 className="font-bold">Explorer</h1>
+        <header>
+          <h1 className="font-bold">Explorer</h1>
+
+          <div>
+            <select
+              name="Filter by region"
+              value={selectedRegion}
+              onChange={(e) => handleRegionSelect(e)}
+              id=""
+            >
+              <option value="all">All</option>
+              <option value="America">America</option>
+              <option value="Oceania">Oceania</option>
+              <option value="Asia">Asia</option>
+            </select>
+          </div>
+        </header>
       </section>
       <section>
         <ul>
           {data &&
             data.map((c: any, idxC: number) => (
-              <li key={`country-${c}-${idxC}`}>{c.name.common}</li>
+              <li
+                key={`country-${c}-${idxC}`}
+                onClick={() => handleCountryClick(c.name.common)}
+                className={`${selectedCountry === c.name.common ? "text-blue-400" : ""
+                  }`}
+              >
+                {c.name.common}
+              </li>
             ))}
         </ul>
       </section>
