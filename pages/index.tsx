cb0c1b51fa3c554997db0cd06ebf3abfc090cac0
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-16 11:34:08 +0530

    feat: capture country guess in accordion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
+import { AccordianGuesses } from "@/components/accordion-guesses";
 import Layout from "@/components/layout";
-import Search from "@/components/search";
-import SelectRegion from "@/components/select-region";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Heading } from "@/components/ui/typography";
 import { API_BASE_URL } from "@/lib/constants";
-import { Region } from "@/lib/enums";
 import { useCountrySearch } from "@/lib/hooks/use-country-search";
 import { useSearchCount } from "@/lib/hooks/use-search-count";
 import { useCountryStore } from "@/lib/state/country-store";
@@ -15,15 +13,9 @@ import { SearchResult } from "@/lib/types/types-fuse-search-result";
 import { cn, fetcher } from "@/lib/utils";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { useQuery } from "@tanstack/react-query";
-import {
-  SearchIcon,
-  ShieldCloseIcon,
-  SidebarCloseIcon,
-  XIcon,
-} from "lucide-react";
+import { SearchIcon } from "lucide-react";
 import { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 const GUESS_LIMIT = 5; // total six guesses. 0..=5
 
@@ -44,10 +36,9 @@ type HomePageProps = {
 // FIX: Use data from local storage or a json file as backup.
 const HomePage: NextPage<HomePageProps> = ({ countries }: HomePageProps) => {
   const [guessCount, setGuessCount] = useState(0);
-
   const { selectedCountry } = useCountryStore();
-
   const [randomCountry, setRandomCountry] = useState<null | ICountry>(null);
+  const [cache, setCache] = useState<string[]>([]);
 
   const {
     data: cachedCountries, // use default value if there's no cached data yet.
@@ -67,45 +58,73 @@ const HomePage: NextPage<HomePageProps> = ({ countries }: HomePageProps) => {
     }
   }, [cachedCountries]);
 
-  function handleInputGuess() {
-    if (guessCount < GUESS_LIMIT) {
-      setGuessCount((prev) => prev + 1);
-      if (selectedCountry === randomCountry?.alpha3Code) {
-        alert("You guessed right!");
-      }
-    } else {
-      alert("You have guessed too many times. Please try again.");
-      return;
+  useEffect(() => {
+    if (selectedCountry === randomCountry?.alpha3Code) {
+      alert("You guessed right!");
     }
-  }
+
+    // setCache((prev) => {
+    //   if (prev[cache.length] !== selectedCountry && selectedCountry !== null) {
+    //     return [...prev, selectedCountry];
+    //   }
+    //   return [...prev];
+    // });
+
+    // if (cache !== null && cache.length === GUESS_LIMIT) {
+    //   alert("You have guessed too many times. Please try again.");
+    // }
+
+    // function handleInputGuess() {
+    //   if (guessCount < GUESS_LIMIT) {
+    //     setGuessCount((prev) => prev + 1);
+    //     if (selectedCountry === randomCountry?.alpha3Code) {
+    //       alert("You guessed right!");
+    //     }
+    //   }
+    //   if (guessCount === GUESS_LIMIT) {
+    //     alert(
+    //       JSON.stringify([
+    //         GUESS_LIMIT,
+    //         guessCount,
+    //         "You have guessed too many times. Please try again.",
+    //       ])
+    //     );
+    //   }
+    // }
+    // handleInputGuess();
+  }, [guessCount, cache, randomCountry, selectedCountry]);
 
   return (
     <Layout title="Home">
-      <section className="gap-4 grid justify-center">
+      <section className="grid gap-4 justify-center">
         <Heading
           color={"default"}
           fontWeight={"bold"}
           variant="h1"
-          className="flex uppercase justify-center"
+          className="flex justify-center uppercase"
         >
           Guess the country
         </Heading>
 
         <div className="py-2">
-          <div className="w-[250px] mx-auto">
+          <div className="mx-auto w-[250px]">
             <AspectRatio ratio={16 / 9}>
               <Image
                 className={cn(
-                  ` rounded-md w-[250px] aspect-video object-cover`
+                  ` rounded-md  shadow w-[250px] aspect-video object-cover`
                 )}
                 fill
                 src={randomCountry?.flag ?? ""}
                 alt={randomCountry?.name ?? ""}
               />
             </AspectRatio>
+          {randomCountry?.name ?? ""}
           </div>
         </div>
-        <HeroSearchBar />
+
+        <>
+          <HeroSearchBar />
+        </>
       </section>
     </Layout>
   );
@@ -115,7 +134,7 @@ export default HomePage;
 
 export function HeroSearchBar() {
   const [guess, setGuess] = useState<ICountry["alpha3Code"]>("");
-  const [guesses, setGuesses] = useState([] as typeof guess[]);
+  const [guesses, setGuesses] = useState([] as (typeof guess)[]);
 
   const [searchCount, setSearchCount] = useState<number>(0);
   const { setSelectedCountry } = useCountryStore();
@@ -168,106 +187,101 @@ export function HeroSearchBar() {
         <div className="flex flex-col mx-auto justify-between items-center p-4 rounded-lg border! border-gray-200 shadow-sm sm:flex-row dark:bg-gray-800 dark:border-gray-700 bg-gray-45">
           <div className="flex-shrink-0 w-full sm:flex sm:w-auto">
             <div className="relative  gap-4  flex-shrink-0 mb-4 w-full sm:mr-4 sm:mb-0 sm:w-64! lg:w-96!">
-              <div className="flex absolute top-0! inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <SearchIcon
-                  aria-hidden="true"
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                />
-              </div>
-
-              <form action="search" className="relative">
-                <label htmlFor="search" className="sr-only">
-                  Search countries:
-                </label>
-                <div className="overflow-y-hidden h-[300px]">
-                  {searchResults !== null &&
-                  searchResults.length > 0 &&
-                  query.length > 1 ? (
-                    <ScrollArea // className="p-4! rounded-md border max-w-fit! min-w-[350px]"
-                    >
-                      <div className="grid">
-                        {searchResults.map((result, idxResult) => {
-                          const country = result.item;
-                          const score = result.score ?? -1;
-                          if (score * 100 <= 4 && score * 100 >= 0) {
-                            return (
-                              <Button
-                                key={`country-search-fuse-${country.alpha3Code}-${idxResult}`}
-                                data-code={country.alpha3Code}
-                                variant={"subtle"}
-                                size={"sm"}
-                                className="justify-start flex"
-                                onClick={(e) =>
-                                  handleCountryClick(e, country.alpha3Code)
-                                }
-                              >
-                                {country.name}
-                                <div
-                                  aria-label="search score rank"
-                                  className="sr-only"
+              <>
+                <form action="search" className="relative">
+                  <label htmlFor="search" className="sr-only">
+                    Search countries:
+                  </label>
+                  <div className="overflow-y-hidden h-[300px]">
+                    {searchResults !== null &&
+                    searchResults.length > 0 &&
+                    query.length > 1 ? (
+                      <ScrollArea // className="p-4! rounded-md border max-w-fit! min-w-[350px]"
+                      >
+                        <div className="grid">
+                          {searchResults.map((result, idxResult) => {
+                            const country = result.item;
+                            const score = result.score ?? -1;
+                            if (score * 100 <= 4 && score * 100 >= 0) {
+                              return (
+                                <Button
+                                  key={`country-search-fuse-${country.alpha3Code}-${idxResult}`}
+                                  data-code={country.alpha3Code}
+                                  variant={"subtle"}
+                                  size={"sm"}
+                                  className="flex justify-start"
+                                  onClick={(
+                                    e: React.MouseEvent<
+                                      HTMLButtonElement,
+                                      MouseEvent
+                                    >
+                                  ) => {
+                                    e.preventDefault();
+                                    handleCountryClick(e, country.alpha3Code);
+                                  }}
                                 >
-                                  {(score * 100).toFixed(0)}
-                                </div>
-                              </Button>
-                            );
-                          } else {
-                            return null;
-                          }
-                        })}
-                      </div>
-                    </ScrollArea>
-                  ) : (
-                    <>
-                      <div className="max-h-[300px] gap-0 w-full grid rounded-md overflow-clip">
-                        {Array.from(Array(GUESS_LIMIT)).map(
-                          (guess, idxGuess) => (
-                            <Button
-                              key={`guess-${idxGuess}`}
-                              className="rounded-none"
-                            >
-                              <span className="sr-only">
-                                Guess number: {idxGuess}{" "}
-                              </span>
-                              {guesses[idxGuess] ?? (
-                                <span className="text-red-400">{guess}</span>
-                              )}
-                            </Button>
-                          )
-                        )}
-                      </div>
-                    </>
-                  )}
-                </div>
-                <div className="relative flex items-center">
-                  <Input
-                    type="search"
-                    placeholder="Search a country…"
-                    autoFocus={true}
-                    onChange={(e) => handleInputOnChange(e)}
-                    className={cn(
-                      "min-w-[60vw] md:min-w-[45vw]" +
-                        "block! p-2.5 py-2 pl-10 w-full! text-sm text-gray-900 bg-white rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  {country.name}
+                                  <div
+                                    aria-label="search score rank"
+                                    className="sr-only"
+                                  >
+                                    {(score * 100).toFixed(0)}
+                                  </div>
+                                </Button>
+                              );
+                            } else {
+                              return null;
+                            }
+                          })}
+                        </div>
+                      </ScrollArea>
+                    ) : (
+                      <>
+                        <div className="grid gap-0 w-full text-sm rounded-md max-h-[300px] overflow-clip">
+                          <AccordianGuesses
+                            guessLimit={GUESS_LIMIT}
+                            guesses={guesses}
+                          />
+                        </div>
+                      </>
                     )}
-                  />
-                  <Button
-                    aria-label="Clear search input"
-                    onClick={() => setQuery("")}
-                    variant={"ghost"}
-                    size="sm"
-                    className="absolute right-0 scale-[80%] opacity-50"
-                  >
-                    <XIcon />
-                  </Button>
-                </div>
-              </form>
+                  </div>
+                  <div className="flex relative items-center">
+                    <div className="flex absolute top-0! inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <SearchIcon
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      />
+                    </div>
+                    <Input
+                      type="search"
+                      placeholder="Search a country…"
+                      value={query}
+                      // NOTE: check for off-by-one error here.
+                      // TODO: Disable selecting a country which has occured before.
+                      disabled={guesses.length === GUESS_LIMIT}
+                      autoFocus={true}
+                      // onBlur={() => setQuery("")}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        // e.preventDefault();
+                        handleInputOnChange(e);
+                      }}
+                      className={cn(
+                        "min-w-[60vw] md:min-w-[45vw]" +
+                          "block! p-2.5 py-2 pl-10 w-full! text-sm text-gray-900 bg-white rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      )}
+                    />
+                  </div>
+                </form>
+              </>
             </div>
           </div>
         </div>
       </div>
       <div
         className={`${
-          searchCount === 0 && "hidden"
-        } text-center text-sm text-gray-600 sm:block dark:text-gray-400`}
+          searchCount === 0 && guesses.length === 0 && "hidden"
+        } text-center text-sm text-gray-600 dark:text-gray-400`}
       >
         Showing {searchCount} results.
       </div>
