18e2193fe917f5280414582327dc667e36365ffb
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-19 18:46:11 +0530

    feat: add card component

--- components/country/country-card.tsx
+++ components/country/country-card.tsx
@@ -1,8 +1,17 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Indicator } from "@/components/ui/indicator";
 import { useCountryStore } from "@/lib/state/country-store";
 import { ICountry } from "@/lib/types/types-country";
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Indicator } from "@/components/ui/indicator";
+import Link from "next/link";
 
 // Component that renders a single country as a card in a grid
 type CountryCardProps = {
@@ -11,42 +20,88 @@ type CountryCardProps = {
 
 export function CountryCard({ country }: CountryCardProps) {
   const { selectedCountry, setSelectedCountry } = useCountryStore();
+
   // Handle click on a country link.
   const handleCountryClick = (alpha3Code: string) => {
     setSelectedCountry(alpha3Code);
   };
   return (
-    <Card
-      padding="none"
-      paddingBody="p-4"
-      linkHref={`/countries/${country.alpha3Code}`}
-      // action={
-      //   <Button variant={"ghost"} className="ms-auto">
-      //     Click Me
-      //   </Button>
-      // }
-      onClick={(_e) => handleCountryClick(country.alpha3Code)}
-      isActive={selectedCountry === country.alpha3Code}
-      activeIndicator={
-        <div className="absolute -top-2 right-3">
-          <Indicator />
-        </div>
-      }
-      title={country.name}
-      description={
-        <div className="grid stats">
-          <div className="flex gap-2 stat">
-            <div className="font-bold">Population</div>
-            <div className="">{country.population}</div>
+    <Link
+      href={`/countries/${country.alpha3Code}`}
+      onClick={handleCountryClick.bind(null, country.alpha3Code)}
+    >
+      <div className="relative">
+        {selectedCountry === country.alpha3Code ? (
+          <div className="absolute -top-2 right-3">
+            <Indicator />
           </div>
-          <div className="flex gap-2 stat">
-            <div className="font-bold">Capital</div>
-            <div className="">{country.capital}</div>
+        ) : null}
+      </div>
+      <Card>
+        <AspectRatio ratio={16 / 9}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            className="object-cover rounded-md opacity-90 hover:opacity-100 w-[600px] md:w-[450px] aspect-video"
+          />
+        </AspectRatio>
+        <CardHeader>
+          <CardTitle>{country.name}</CardTitle>
+          {/* <CardDescription>Card Description</CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <div className="grid stats">
+            <div className="flex gap-2 stat">
+              <div className="font-bold">Population</div>
+              <div className="">{country.population}</div>
+            </div>
+            <div className="flex gap-2 stat">
+              <div className="font-bold">Capital</div>
+              <div className="">{country.capital}</div>
+            </div>
+            <div className="flex gap-2 stat">
+              <div className="font-bold">TLD</div>
+              {/* <div className="text-xs!">{country.timezones.join(" ")}</div> */}
+              <div className="text-xs!">{country.topLevelDomain.join(" ")}</div>
+            </div>
           </div>
-        </div>
-      }
-      imageUrl={country.flag}
-      imageAlt={`Flag of ${country.name}`}
-    />
+        </CardContent>
+        {/* <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter> */}
+      </Card>
+    </Link>
+    // <Card
+    //   padding="none"
+    //   paddingBody="p-4"
+    //   linkHref={`/countries/${country.alpha3Code}`}
+    //   // action={
+    //   //   <Button variant={"ghost"} className="ms-auto">
+    //   //     Click Me
+    //   //   </Button>
+    //   // }
+    //   onClick={(_e) => handleCountryClick(country.alpha3Code)}
+    //   isActive={selectedCountry === country.alpha3Code}
+    //   activeIndicator={
+    //     <div className="absolute -top-2 right-3">
+    //       <Indicator />
+    //     </div>
+    //   }
+    //   title={country.name}
+    //   description={
+    //     <div className="grid stats">
+    //       <div className="flex gap-2 stat">
+    //         <div className="font-bold">Population</div>
+    //         <div className="">{country.population}</div>
+    //       </div>
+    //       <div className="flex gap-2 stat">
+    //         <div className="font-bold">Capital</div>
+    //         <div className="">{country.capital}</div>
+    //       </div>
+    //     </div>
+    //   }
+    //   imageUrl={country.flag}
+    //   imageAlt={`Flag of ${country.name}`}
+    // />
   );
 }
