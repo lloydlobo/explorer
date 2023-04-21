ea78342177cfdf122f61deb104dfd62516f117ef
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-21 09:58:22 +0530

    build: enable vercel analytics edge functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { Provider } from "jotai";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
@@ -37,6 +38,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <ClerkProvider {...pageProps}>
               <Component {...pageProps} />
               <Toaster />
+              <Analytics />
             </ClerkProvider>
           </ThemeProvider>
           <ReactQueryDevtools initialIsOpen={false} />
